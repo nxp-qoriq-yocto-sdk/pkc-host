nxp-qoriq-yocto-sdk/pkc-host

commit 63fee79ab107d8e960fe530333c795c51deb35d9

debug_print: fix compiler warnings, clean-up messages
Note: modprobe/rmmod issue where driver fails to load when debug print is
enabled remains

Signed-off-by: Cristian Stoica <cristian.stoica@freescale.com>
Change-Id: I95eb13aa8c71952c3ca4f671b0c9a216459f95c3
Reviewed-on: http://git.am.freescale.net:8181/37434

apps/cli/cli.c
@@ -354,12 +354,12 @@ int exe_debug_command()
                         for (i=0; i<DEBUG_COL_COUNT; ++i){
 				ASSIGN32(cmd.op_buffer->debug_op[k], cmd.op_buffer->debug_op[k]);
                             if(!i)
-                                printf("\n%08x  :\t",(unsigned int)((unsigned int *)cmd.rsrc.dgb.address + 4*j));
+                                printf("%08x  :\t",(unsigned int)((unsigned int *)cmd.rsrc.dgb.address + 4*j));
                             else
                                 printf("%08x\t", cmd.op_buffer->debug_op[k++]);
                         }
                     }
-                    printf("\n\n");
+                    printf("\n");
                 }
 				break;
 
@@ -432,9 +432,9 @@ int exe_command()
 				ret = ioctl(fd, CHECKCMD, &cmd);
 
 				if( -1 == ret)
-					printf("\nOOPS ... invalid dev_id \n\n");
+					printf("OOPS ... invalid dev_id \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
 					debug(cmd.dev_id);
 
@@ -448,9 +448,9 @@ int exe_command()
 				ret = ioctl(fd, CMDOPERATION , &cmd);
 
                 if (-1 == ret)
-                    printf("\nOOPS ... invalid dev_id \n\n");
+                    printf("OOPS ... invalid dev_id \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
                     if (!*(cmd.result))
                     {
@@ -467,7 +467,7 @@ int exe_command()
 
 						fwversion = cmd.op_buffer->dev_stat_op.fwversion;
 
-						printf("\nDEVICE STATISTICS\n");
+						printf("DEVICE STATISTICS\n");
 						printf("DEVICE ID                   :%d\n",cmd.dev_id);
 						printf("FIRMWARE VERSION            :%u/%u/20%u -- V %u.%u\n",(fwversion & DATE_MASK)>>DATE_SHIFT,
                                                                                       (fwversion & MONTH_MASK)>>MONTH_SHIFT,
@@ -486,7 +486,7 @@ int exe_command()
 	                                                         cmd.op_buffer->dev_stat_op.total_jobs_pending );
                     }
                     else
-						printf("\nOOPS ... something went wrong\n\n");
+						printf("OOPS ... something went wrong\n\n");
 
 				free(cmd.result);
                 free(cmd.op_buffer);
@@ -500,19 +500,19 @@ int exe_command()
 				ret = ioctl(fd, CHECKCMD, &cmd);
 
 				if (-1 == ret)
-					printf("\nOOPS ... invalid parameters \n\n");
+					printf("OOPS ... invalid parameters \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
 				{
-					printf("\nDOING HANDSHAKE FOR DEVICE :%d WITH CONFIG FILE :%s \n",cmd.dev_id,cmd.rsrc.config);
+					printf("DOING HANDSHAKE FOR DEVICE :%d WITH CONFIG FILE :%s \n",cmd.dev_id,cmd.rsrc.config);
 					if (-1 == ioctl(fd, CMDOPERATION , &cmd))
 						printf("OOPS ... invalid dev_id \n\n");
 					else
 						if (!*(cmd.result))
-							printf("\nHANDSHAKE SUCCESSFULL\n\n");
+							printf("HANDSHAKE SUCCESSFULL\n\n");
 						else
-							printf("\nOOPS ... something went wrong... Device is Down\n\n");
+							printf("OOPS ... something went wrong... Device is Down\n\n");
 				}
 				free(cmd.result);
 				return 0;
@@ -525,16 +525,16 @@ int exe_command()
 				ret = ioctl(fd, CMDOPERATION , &cmd);
 
                 if (-1 == ret)
-					printf("\nOOPS ... invalid dev_id \n\n");
+					printf("OOPS ... invalid dev_id \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
 				{
 					ASSIGN32(cmd.op_buffer->ping_op.resp, cmd.op_buffer->ping_op.resp);
 	                if (ALIVE == cmd.op_buffer->ping_op.resp)
-	                printf("\nDEVICE IS ALIVE\n\n");
+	                printf("DEVICE IS ALIVE\n\n");
 		        else
-					    printf("\nDEVICE IS DEAD\n\n");
+					    printf("DEVICE IS DEAD\n\n");
 				}
 				free(cmd.result);
                 free(cmd.op_buffer);
@@ -547,14 +547,14 @@ int exe_command()
 				ret = ioctl(fd, CMDOPERATION , &cmd);
 
                 if (-1 == ret)
-                    printf("\nOOPS ... invalid dev_id \n\n");
+                    printf("OOPS ... invalid dev_id \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
 					if (!*(cmd.result))
-	                printf("\nDEVICE HAS BEEN RESET\n\n");
+	                printf("DEVICE HAS BEEN RESET\n\n");
 		        else
-		        printf("\nOOPS ... something went wrong\n\n");
+		        printf("OOPS ... something went wrong\n\n");
 
 	            free(cmd.result);
 				return 0;
@@ -566,14 +566,14 @@ int exe_command()
 				ret = ioctl(fd, CMDOPERATION , &cmd);
 
                 if (-1 == ret)
-                    printf("\nOOPS ... invalid dev_id/sec_id \n\n");
+                    printf("OOPS ... invalid dev_id/sec_id \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
 					if (!*(cmd.result))
-	                printf("\nSEC ENGINE HAS BEEN RESET\n\n");
+	                printf("SEC ENGINE HAS BEEN RESET\n\n");
 		        else
-		        printf("\nOOPS ... something went wrong\n\n");
+		        printf("OOPS ... something went wrong\n\n");
 
 				free(cmd.result);
                 return 0;
@@ -587,9 +587,9 @@ int exe_command()
 				ret = ioctl(fd, CMDOPERATION , &cmd);
 
                 if (-1 == ret)
-                    printf("\nOOPS ... invalid dev_id/ring_id \n\n");
+                    printf("OOPS ... invalid dev_id/ring_id \n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
 				else
 	                if (!*(cmd.result))
 	                {
@@ -602,7 +602,7 @@ int exe_command()
 						ASSIGN32(cmd.op_buffer->ring_stat_op.jobs_processed, cmd.op_buffer->ring_stat_op.jobs_processed);
 						ASSIGN32(cmd.op_buffer->ring_stat_op.jobs_pending, cmd.op_buffer->ring_stat_op.jobs_pending);
 
-						printf("\nRING STATISTICS\n");
+						printf("RING STATISTICS\n");
 						printf("DEVICE ID                   :%d\n",cmd.dev_id);
 						printf("RING ID                     :%d\n",cmd.rsrc.ring_id);
 						printf("RING SIZE                   :%u\n",cmd.op_buffer->ring_stat_op.tot_size);
@@ -615,7 +615,7 @@ int exe_command()
 						printf("JOBS PENDING                :%u\n\n",cmd.op_buffer->ring_stat_op.jobs_pending);
 		        }
 		    else
-			    printf("\nOOPS ... something went wrong\n\n");
+			    printf("OOPS ... something went wrong\n\n");
 
                 free(cmd.op_buffer);
 				free(cmd.result);
@@ -630,9 +630,9 @@ int exe_command()
 				ret = ioctl(fd, CMDOPERATION , &cmd);
 
                 if (-1 == ret)
-                    printf("\nOOPS ... invalid dev_id\n\n");
+                    printf("OOPS ... invalid dev_id\n\n");
 				else if (EACCES == ret)
-					printf("\nCLI is disabled.. HIGH PERF mode is defined \n\n");
+					printf("CLI is disabled.. HIGH PERF mode is defined \n\n");
                 else
 			if (!*(cmd.result))
                     {
@@ -640,7 +640,7 @@ int exe_command()
 						ASSIGN32(cmd.op_buffer->sec_op.sec_ver, cmd.op_buffer->sec_op.sec_ver);
 						ASSIGN32(cmd.op_buffer->sec_op.no_of_sec_engines, cmd.op_buffer->sec_op.no_of_sec_engines);
 						ASSIGN32(cmd.op_buffer->sec_op.no_of_sec_jr, cmd.op_buffer->sec_op.no_of_sec_jr);
-                        printf("\nSEC STATISTICS\n");
+                        printf("SEC STATISTICS\n");
                         printf("SEC VERSION                 :%x.%x\n",(cmd.op_buffer->sec_op.sec_ver & 0x0000ff00)>>8,
                                                                       (cmd.op_buffer->sec_op.sec_ver & 0x000000ff));
                         printf("NO OF SEC ENGINES           :%d\n",cmd.op_buffer->sec_op.no_of_sec_engines);
@@ -653,10 +653,10 @@ int exe_command()
 				printf("\tSEC JOBS ADDED      :%u\n",cmd.op_buffer->sec_op.sec[i].sec_tot_req_jobs);
 				printf("\tSEC JOBS PROCESSED  :%u\n",cmd.op_buffer->sec_op.sec[i].sec_tot_resp_jobs);
 			}
-			printf("\n");
+			printf("");
 			}
                     else
-                        printf("\nOOPS ... something went wrong\n\n");
+                        printf("OOPS ... something went wrong\n\n");
 
                 free(cmd.op_buffer);
                 free(cmd.result);

algs/dh.c
@@ -147,7 +147,7 @@ static int dh_key_cp_req(struct dh_key_req_s *req, crypto_mem_info_t *mem_info,
 	dh_key_init_len(req, mem_info, ecdh);
 
 	/* Alloc mem requrd for crypto operation */
-	print_debug("\t \t Calling alloc_crypto_mem\n");
+	print_debug("Calling alloc_crypto_mem\n");
 	if (-ENOMEM == alloc_crypto_mem(mem_info))
 		return -ENOMEM;
 #ifdef USE_HOST_DMA
@@ -179,7 +179,7 @@ static int dh_keygen_cp_req(struct dh_keygen_req_s *req, crypto_mem_info_t *mem_
     dh_keygen_init_len(req, mem_info, ecdh);
 
     /* Alloc mem requrd for crypto operation */
-    print_debug("\t \t Calling alloc_crypto_mem \n \n");
+    print_debug("Calling alloc_crypto_mem\n");
     if(-ENOMEM == alloc_crypto_mem(mem_info))
         return -ENOMEM;
 #ifdef USE_HOST_DMA
@@ -245,20 +245,17 @@ static void constr_dh_key_desc(crypto_mem_info_t *mem_info)
 
 #ifdef DUMP_DEBUG_V_INFO
 
-	print_debug("Q DMA			:%0llx\n",
-		    mem->q_buff.dev_buffer.d_p_addr);
-	print_debug("W DMA			:%0llx\n",
-		    mem->w_buff.dev_buffer.d_p_addr);
-	print_debug("S DMA			:%0llx\n",
-		    mem->s_buff.dev_buffer.d_p_addr);
-	print_debug("Z DMA          :%0llx\n", mem->z_buff.dev_buffer.d_p_addr);
+	print_debug("Q DMA: %llx\n", (uint64_t)mem->q_buff.dev_buffer.d_p_addr);
+	print_debug("W DMA: %llx\n", (uint64_t)mem->w_buff.dev_buffer.d_p_addr);
+	print_debug("S DMA: %llx\n", (uint64_t)mem->s_buff.dev_buffer.d_p_addr);
+	print_debug("Z DMA: %llx\n", (uint64_t)mem->z_buff.dev_buffer.d_p_addr);
 
-	print_debug("[DH]	Descriptor words\n");
+	print_debug("[DH] Descriptor words\n");
 	{
 		uint32_t *words = (uint32_t *) desc_buff;
 		uint32_t i = 0;
 		for (i = 0; i < desc_size; i++)
-			print_debug("Word %d	:	%0x\n", i, words[i]);
+			print_debug("Word %d: %x\n", i, words[i]);
 	}
 #endif
 }
@@ -308,20 +305,18 @@ static void constr_ecdh_key_desc(crypto_mem_info_t *mem_info, bool ecc_bin)
 			  OP_PCL_PKPROT_ECC));
 
 #ifdef DUMP_DEBUG_V_INFO
+	print_debug("Q DMA: %llx\n", (uint64_t)mem->q_buff.dev_buffer.d_p_addr);
+	print_debug("W DMA: %llx\n", (uint64_t)mem->w_buff.dev_buffer.d_p_addr);
+	print_debug("S DMA: %llx\n", (uint64_t)mem->s_buff.dev_buffer.d_p_addr);
+	print_debug("Z DMA: %llx\n", (uint64_t)mem->z_buff.dev_buffer.d_p_addr);
+	print_debug("AB DMA: %llx\n", (uint64_t)mem->ab_buff.dev_buffer.d_p_addr);
 
-	print_debug("Q DMA          :%0llx\n", mem->q_buff.dev_buffer.d_p_addr);
-	print_debug("W DMA          :%0llx\n", mem->w_buff.dev_buffer.d_p_addr);
-	print_debug("S DMA          :%0llx\n", mem->s_buff.dev_buffer.d_p_addr);
-	print_debug("Z DMA          :%0llx\n", mem->z_buff.dev_buffer.d_p_addr);
-	print_debug("AB DMA          :%0llx\n",
-		    mem->ab_buff.dev_buffer.d_p_addr);
-
-	print_debug("\n [ECDH]  Descriptor words\n");
+	print_debug("[ECDH] Descriptor words\n");
 	{
 		uint32_t *words = (uint32_t *) desc_buff;
 		uint32_t i = 0;
 		for (i = 0; i < desc_size; i++)
-			print_debug("Word %d    :   %0x\n", i, words[i]);
+			print_debug("Word %d: %x\n", i, words[i]);
 	}
 #endif
 }
@@ -370,18 +365,18 @@ static void constr_ecdh_keygen_desc(crypto_mem_info_t *mem_info, bool ecc_bin)
 
 #ifdef DUMP_DEBUG_V_INFO
 
-    print_debug("Q DMA          :%0llx \n", mem->q_buff.dev_buffer.d_p_addr);
-    print_debug("R DMA          :%0llx \n", mem->r_buff.dev_buffer.d_p_addr);
-    print_debug("G DMA          :%0llx \n", mem->g_buff.dev_buffer.d_p_addr);
-    print_debug("PUBKEY DMA     :%0llx \n", mem->pubkey_buff.dev_buffer.d_p_addr);
-    print_debug("PRVKEY DMA     :%0llx \n", mem->prvkey_buff.dev_buffer.d_p_addr);
-    print_debug("AB DMA          :%0llx \n", mem->ab_buff.dev_buffer.d_p_addr);
-    print_debug("\n [ECDH]  Descriptor words \n");
+    print_debug("Q DMA: %llx\n", (uint64_t)mem->q_buff.dev_buffer.d_p_addr);
+    print_debug("R DMA: %llx\n", (uint64_t)mem->r_buff.dev_buffer.d_p_addr);
+    print_debug("G DMA: %llx\n", (uint64_t)mem->g_buff.dev_buffer.d_p_addr);
+    print_debug("PUBKEY DMA: %llx\n", (uint64_t)mem->pubkey_buff.dev_buffer.d_p_addr);
+    print_debug("PRVKEY DMA: %llx\n",(uint64_t) mem->prvkey_buff.dev_buffer.d_p_addr);
+    print_debug("AB DMA: %llx\n", (uint64_t)mem->ab_buff.dev_buffer.d_p_addr);
+    print_debug("[ECDH] Descriptor words\n");
     {
         uint32_t    *words = (uint32_t *)desc_buff;
         uint32_t    i   =   0;
         for(i=0; i<desc_size; i++)
-            print_debug("Word %d    :   %0x \n", i, words[i]);
+            print_debug("Word %d: %x\n", i, words[i]);
     }
 #endif
 }
@@ -419,19 +414,18 @@ static void constr_dh_keygen_desc(crypto_mem_info_t *mem_info)
 
 #ifdef DUMP_DEBUG_V_INFO
 
-    print_debug("Q DMA          :%0llx \n", mem->q_buff.dev_buffer.d_p_addr);
-    print_debug("R DMA          :%0llx \n", mem->r_buff.dev_buffer.d_p_addr);
-    print_debug("G DMA          :%0llx \n", mem->g_buff.dev_buffer.d_p_addr);
-    print_debug("PUBKEY DMA     :%0llx \n", mem->pubkey_buff.dev_buffer.d_p_addr);
-    print_debug("PRVKEY DMA     :%0llx \n", mem->prvkey_buff.dev_buffer.d_p_addr);
+    print_debug("Q DMA: %llx\n", (uint64_t)mem->q_buff.dev_buffer.d_p_addr);
+    print_debug("R DMA: %llx\n", (uint64_t)mem->r_buff.dev_buffer.d_p_addr);
+    print_debug("G DMA: %llx\n", (uint64_t)mem->g_buff.dev_buffer.d_p_addr);
+    print_debug("PUBKEY DMA: %llx\n", (uint64_t)mem->pubkey_buff.dev_buffer.d_p_addr);
+    print_debug("PRVKEY DMA: %llx\n", (uint64_t)mem->prvkey_buff.dev_buffer.d_p_addr);
 
-
-    print_debug("\n [DH]    Descriptor words \n");
+    print_debug("[DH] Descriptor words\n");
     {
         uint32_t    *words = (uint32_t *)desc_buff;
         uint32_t    i   =   0;
         for(i=0; i<desc_size; i++)
-            print_debug("Word %d    :   %0x \n", i, IO_BE_READ32(&words[i]));
+            print_debug("Word %d: %x\n", i, IO_BE_READ32(&words[i]));
     }
 #endif
 }
@@ -543,8 +537,7 @@ int dh_op(struct pkc_request *req)
 #endif
 
 	crypto_ctx = get_crypto_ctx(c_dev->ctx_pool);
-	print_debug("\t crypto_ctx addr :			:%0llx\n",
-		    crypto_ctx);
+	print_debug("crypto_ctx addr: %p\n", crypto_ctx);
 
 	if (unlikely(!crypto_ctx)) {
 		print_error("Mem alloc failed....\n");
@@ -552,12 +545,11 @@ int dh_op(struct pkc_request *req)
 		goto error;
 	}
 
-	print_debug("\t Ring selected			:%d\n", r_id);
+	print_debug("Ring selected: %d\n", r_id);
 	crypto_ctx->ctx_pool = c_dev->ctx_pool;
 	crypto_ctx->crypto_mem.dev = c_dev;
 	crypto_ctx->crypto_mem.pool = c_dev->ring_pairs[r_id].ip_pool;
-	print_debug("\t IP Buffer pool address		:%0x\n",
-		    crypto_ctx->crypto_mem.pool);
+	print_debug("IP Buffer pool address: %p\n", crypto_ctx->crypto_mem.pool);
 
 	if (ECDH_COMPUTE_KEY == req->type || ECDH_KEYGEN == req->type) {
 		ecdh = true;
@@ -576,12 +568,12 @@ int dh_op(struct pkc_request *req)
 				ret = -ENOMEM;
                 goto error;
 			}
-            print_debug("\t \t \t DH init mem complete..... \n");
+            print_debug("DH init mem complete..... \n");
 
             /* Convert the buffers to dev */
             host_to_dev(&crypto_ctx->crypto_mem);
 
-            print_debug("\t \t \t Host to dev convert complete.... \n");
+            print_debug("Host to dev convert complete.... \n");
 
 #ifdef SEC_DMA
             map_crypto_mem(&(crypto_ctx->crypto_mem));
@@ -592,7 +584,7 @@ int dh_op(struct pkc_request *req)
                 constr_ecdh_keygen_desc(&crypto_ctx->crypto_mem, ecc_bin);
             else
                 constr_dh_keygen_desc(&crypto_ctx->crypto_mem);
-            print_debug("\t \t \t Desc constr complete... \n");
+            print_debug("Desc constr complete... \n");
 
 #ifdef SEC_DMA
             sec_dma = dh_keygen_buffs->desc_buff.dev_buffer.h_p_addr + offset;
@@ -601,14 +593,14 @@ int dh_op(struct pkc_request *req)
 #endif
 
             /* Store the context */
-            print_debug(KERN_ERR "[Enq] Desc addr   :%0llx Hbuffer addr     :%0x    Crypto ctx      :%0x \n",
-                                                dh_keygen_buffs->desc_buff.dev_buffer.d_p_addr,
-                                                dh_keygen_buffs->desc_buff.v_mem, crypto_ctx);
+            print_debug("[Enq] Desc addr: %llx Hbuffer addr: %p    Crypto ctx: %p \n",
+		(uint64_t)dh_keygen_buffs->desc_buff.dev_buffer.d_p_addr,
+		dh_keygen_buffs->desc_buff.v_mem, crypto_ctx);
 
             store_priv_data(crypto_ctx->crypto_mem.pool, dh_keygen_buffs->desc_buff.v_mem, (unsigned long)crypto_ctx);
 
             break;
-                
+
 	case DH_COMPUTE_KEY:
 	case ECDH_COMPUTE_KEY:
 		dh_key_init_crypto_mem(&crypto_ctx->crypto_mem, ecdh);
@@ -621,12 +613,12 @@ int dh_op(struct pkc_request *req)
 			ret = -ENOMEM;
 			goto error;
 		}
-		print_debug("\t \t \t DH init mem complete.....\n");
+		print_debug("DH init mem complete.....\n");
 
 		/* Convert the buffers to dev */
 		host_to_dev(&crypto_ctx->crypto_mem);
 
-		print_debug("\t \t \t Host to dev convert complete....\n");
+		print_debug("Host to dev convert complete....\n");
 
 #ifdef SEC_DMA
             map_crypto_mem(&(crypto_ctx->crypto_mem));
@@ -637,7 +629,7 @@ int dh_op(struct pkc_request *req)
 			constr_ecdh_key_desc(&crypto_ctx->crypto_mem, ecc_bin);
 		else
 			constr_dh_key_desc(&crypto_ctx->crypto_mem);
-		print_debug("\t \t \t Desc constr complete...\n");
+		print_debug("Desc constr complete...\n");
 
 #ifdef SEC_DMA
                 sec_dma = dh_key_buffs->desc_buff.dev_buffer.h_p_addr + offset;
@@ -646,9 +638,8 @@ int dh_op(struct pkc_request *req)
 #endif
 
 		/* Store the context */
-		print_debug(KERN_ERR
-			    "[Enq] Desc addr :%0llx Hbuffer addr :%0x	Crypto ctx :%0x\n",
-			    dh_key_buffs->desc_buff.dev_buffer.d_p_addr,
+		print_debug("[Enq] Desc addr: %llx Hbuffer addr: %p	Crypto ctx: %p\n",
+			    (uint64_t)dh_key_buffs->desc_buff.dev_buffer.d_p_addr,
 			    dh_key_buffs->desc_buff.v_mem, crypto_ctx);
 
 		store_priv_data(crypto_ctx->crypto_mem.pool,
@@ -702,7 +693,7 @@ int dh_op(struct pkc_request *req)
 		goto error;
 	}
 #else
-	print_debug(KERN_ERR "Before app_ring_enqueue\n");
+	print_debug("Before app_ring_enqueue\n");
 
 	sec_dma = set_sec_affinity(c_dev, r_id, sec_dma);
 #ifndef HIGH_PERF

commit 93d41be7801dec22dce00ef01a756bf6ede24eac

fix compiler warning: zero-length gnu_printf format string
revert error introduced in 63fee79ab107d8e960fe530333c795c51deb35d9

Signed-off-by: Cristian Stoica <cristian.stoica@freescale.com>
Change-Id: I038b3371177903e6d0e6d75af2f6ac9bca36dfa5
Reviewed-on: http://git.am.freescale.net:8181/38653

apps/cli/cli.c
@@ -653,7 +653,7 @@ int exe_command()
 				printf("\tSEC JOBS ADDED      :%u\n",cmd.op_buffer->sec_op.sec[i].sec_tot_req_jobs);
 				printf("\tSEC JOBS PROCESSED  :%u\n",cmd.op_buffer->sec_op.sec[i].sec_tot_resp_jobs);
 			}
-			printf("");
+			printf("\n");
 			}
                     else
                         printf("OOPS ... something went wrong\n\n");