nxp-qoriq-yocto-sdk/pkc-host

commit 31f7331d96083e08e1855f7cd7a980d0d6b9c8d3

reduce code complexity
- remove offset accumulator
- extract checks for BT_DESC from switch statement

Signed-off-by: Cristian Stoica <cristian.stoica@freescale.com>
Change-Id: I35cc2afc6a9d5f7ead2a53bc344f0ce0d1ae405c
Reviewed-on: http://git.am.freescale.net:8181/34164

algs/desc_cnstr.c
@@ -49,27 +49,28 @@ extern fsl_pci_dev_t *g_fsl_pci_dev;
 
 static void distribute_buffers(crypto_mem_info_t *mem_info, uint8_t *mem)
 {
-	uint32_t i = 0;
-	uint32_t offset = 0;
+	uint32_t i;
 	buffer_info_t *buffers = (buffer_info_t *) &mem_info->c_buffers;
 
-	for (i = 0; i < mem_info->count; i++) {
+	/* BT_DESC is only the first in the array */
+	buffers[0].v_mem = mem;
+	mem += ALIGN_LEN_TO_DMA(buffers[0].len);
+
+	for (i = 1; i < mem_info->count; i++) {
 		switch (buffers[i].bt) {
-		case BT_DESC:
-			buffers[i].v_mem = (mem + offset);
-			offset += ALIGN_LEN_TO_DMA(buffers[i].len);
-			break;
 		case BT_IP:
 			if (!mem_info->split_ip) {
-				buffers[i].v_mem = (mem + offset);
-				offset += ALIGN_LEN_TO_DMA(buffers[i].len);
+				buffers[i].v_mem = mem;
+				mem += ALIGN_LEN_TO_DMA(buffers[i].len);
 			}
 			break;
 		case BT_OP:
 #ifdef OP_BUFFER_IN_DEV_MEM
-			buffers[i].v_mem = (mem + offset);
-			offset += ALIGN_LEN_TO_DMA(buffers[i].len);
+			buffers[i].v_mem = mem;
+			mem += ALIGN_LEN_TO_DMA(buffers[i].len);
 #endif
+			break;
+		default:
 			break;
 		}
 	}

commit 6917577a6d4e4a7d2c7951203e08004aab8eb58e

fix incorrect buffer mem distribution
This fixes a bug introduced in 31f7331d96083e08e1855f7cd7a980d0d6b9c8d3
as a result of an incorrect assumption

Signed-off-by: Cristian Stoica <cristian.stoica@freescale.com>
Change-Id: Ib18269884b38ecfed4aae5df641f19e9f2c14cb1
Reviewed-on: http://git.am.freescale.net:8181/38833

algs/desc_cnstr.c
@@ -52,12 +52,12 @@ static void distribute_buffers(crypto_mem_info_t *mem_info, uint8_t *mem)
 	uint32_t i;
 	buffer_info_t *buffers = (buffer_info_t *) &mem_info->c_buffers;
 
-	/* BT_DESC is only the first in the array */
-	buffers[0].v_mem = mem;
-	mem += ALIGN_LEN_TO_DMA(buffers[0].len);
-
-	for (i = 1; i < mem_info->count; i++) {
+	for (i = 0; i < mem_info->count; i++) {
 		switch (buffers[i].bt) {
+		case BT_DESC:
+			buffers[i].v_mem = mem;
+			mem += ALIGN_LEN_TO_DMA(buffers[i].len);
+			break;
 		case BT_IP:
 			if (!mem_info->split_ip) {
 				buffers[i].v_mem = mem;
@@ -69,8 +69,6 @@ static void distribute_buffers(crypto_mem_info_t *mem_info, uint8_t *mem)
 			buffers[i].v_mem = mem;
 			mem += ALIGN_LEN_TO_DMA(buffers[i].len);
 #endif
-			break;
-		default:
 			break;
 		}
 	}