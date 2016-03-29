6abcb4442f3ca5c5e51e0ca1f6be37a871b63bd8
Cristian Stoica <cristian.stoica@nxp.com>
2016-03-29 11:20:06 +0300

    remove information ignored during handshake
    This field is ignored by the device firmware during handshake. The
    actual ring address is obtained from the host during HS_INIT_RING_PAIR.
    The actual start address for each ring is communicated and not the start
    address for the bulk area reserved for rings
    
    Signed-off-by: Cristian Stoica <cristian.stoica@nxp.com>

--- host_driver/fsl_c2x0_crypto_layer.c
+++ host_driver/fsl_c2x0_crypto_layer.c
@@ -447,7 +447,6 @@ void send_hs_init_config(fsl_crypto_dev_t *dev)
 {
 	const char *str_state = "HS_INIT_CONFIG\n";
 	phys_addr_t host_p_addr = dev->priv_dev->bars[MEM_TYPE_DRIVER].host_p_addr;
-	phys_addr_t drv_resp_rings = dev->ob_mem.drv_resp_rings + host_p_addr;
 	phys_addr_t fw_resp_ring   = dev->ob_mem.fw_resp_ring + host_p_addr;
 	phys_addr_t s_cntrs        = dev->ob_mem.s_c_cntrs_mem + host_p_addr;
 	phys_addr_t r_s_cntrs      = dev->ob_mem.s_c_r_cntrs_mem + host_p_addr;
@@ -464,7 +463,6 @@ void send_hs_init_config(fsl_crypto_dev_t *dev)
 	/* TODO: These iowrite32be truncate 64bit addresses on 64bit machines.
 	 * The DMA space is indeed limited to 32/36 bit but what about the
 	 * physical addresses on the host? */
-	iowrite32be(drv_resp_rings, &config->drv_resp_ring);
 	iowrite32be(fw_resp_ring, &config->fw_resp_ring);
 	iowrite32be(s_cntrs, &config->s_cntrs);
 	iowrite32be(r_s_cntrs, &config->r_s_cntrs);
@@ -473,7 +471,6 @@ void send_hs_init_config(fsl_crypto_dev_t *dev)
 	print_debug("HS_INIT_CONFIG Details\n");
 	print_debug("Num of rps: %d\n", dev->num_of_rings);
 	print_debug("Req mem size: %d\n", dev->tot_req_mem_size);
-	print_debug("Drv resp ring: %pa\n", &drv_resp_rings);
 	print_debug("Fw resp ring: %pa\n", &fw_resp_ring);
 	print_debug("S C Counters: %pa\n", &s_cntrs);
 	print_debug("R S C counters: %pa\n", &r_s_cntrs);
