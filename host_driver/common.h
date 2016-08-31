4b536f76a65a220b8374f9c5ce955392e9a7ac58
Cristian Stoica <cristian.stoica@nxp.com>
2016-08-31 15:21:12 +0300

    take driver specific definitions out of the common.h
    Signed-off-by: Cristian Stoica <cristian.stoica@nxp.com>

--- host_driver/common.h
+++ host_driver/common.h
@@ -66,59 +66,6 @@
 #include <linux/sched.h>
 #include <linux/cpumask.h>
 
-/* Identifier for the ring pairs */
-typedef enum ring_id {
-	CRYPTO_COMMAND_RING_ID,
-	CRYPTO_APP_RING_ID,
-	/*This ID is not used in driver but the same
-	 * enum will be used by firmware*/
-	CRYPTO_SEC_RING_ID
-} ring_id_t;
 
-/*******************************************************************************
-Description :	Contains the configuration read from the file.
-Fields      :	dev_no    : Number of the device to which this config applies.
-		ring_id   : Identifies the ring Command/App
-		flags     : Useful only for App to identify its properties
-			0-4 : Priority level 32- priority levels
-			5-7 : SEC engine affinity
-			8   : Ordered/Un-ordered
-		list      : To maintain list of config structures per device
-*******************************************************************************/
-struct crypto_dev_config {
-	uint32_t dev_no;
-/*  int8_t      *name;  We may not need this field  */
-#define FIRMWARE_FILE_DEFAULT_PATH  "/etc/crypto/pkc-firmware.bin"
-#define FIRMWARE_FILE_PATH_LEN  100
-	uint8_t fw_file_path[FIRMWARE_FILE_PATH_LEN];
-
-	uint8_t *firmware;
-
-	uint8_t num_of_rings;
-
-/* Safe MAX number of ring pairs -
- * Only required for some static data structures. */
-#define FSL_CRYPTO_MAX_RING_PAIRS   6
-
-	struct ring_info {
-		ring_id_t ring_id;
-		uint32_t depth;
-		uint8_t flags;
-		uint32_t msi_addr_l;
-		uint32_t msi_addr_h;
-		uint16_t msi_data;
-	} ring[FSL_CRYPTO_MAX_RING_PAIRS];
-
-	struct list_head list;
-};
-
-/* Different types of memory between driver and ep */
-typedef enum crypto_dev_mem_type {
-	MEM_TYPE_CONFIG,
-	MEM_TYPE_SRAM,
-	MEM_TYPE_DRIVER,
-	MEM_TYPE_MSI,
-	MEM_TYPE_MAX
-} crypto_dev_mem_type_t;
 
 #endif
